3888df18079e5253b04f49ec3f824646f88a2c19
Juan Gomez <juan@juangomez.co>
2017-12-08 14:27:24 +0100

    Change on JS languages

--- js/switch.js
+++ js/switch.js
@@ -1,47 +1,22 @@
 //languages
 function ES() {
-  var showme = document.getElementById("ES");
-  showme.style.display = "block";
-  var hideme = document.getElementById("EN");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("FR");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("DE");
-  hideme.style.display = "none";
+  $("#EN, #FR, #ES").hide();
+  $("#ES").show();
 }
 
 function FR() {
-
-  var showme = document.getElementById("FR");
-  showme.style.display = "block";
-  var hideme = document.getElementById("EN");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("ES");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("DE");
-  hideme.style.display = "none";
+  $("#EN, #DE, #ES").hide();
+  $("#FR").show();
 }
 
 function EN() {
-  var showme = document.getElementById("EN");
-  showme.style.display = "block";
-  var hideme = document.getElementById("FR");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("ES");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("DE");
-  hideme.style.display = "none";
+  $("#FR, #DE, #ES").hide();
+  $("#EN").show();
 }
 
 function DE() {
-  var showme = document.getElementById("DE");
-  showme.style.display = "block";
-  var hideme = document.getElementById("FR");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("ES");
-  hideme.style.display = "none";
-  var hideme = document.getElementById("EN");
-  hideme.style.display = "none";
+  $("#EN, #FR, #ES").hide();
+  $("#DE").show();
 }
 //Open the words
 $(".abrir").click(function() {
@@ -64,25 +39,3 @@ function mover() {
     });
   });
 }
-
-// window.onload = function(){
-//    setTimeout(function(){
-//        mover();
-//    }, 2000);
-// };
-// $(function() {
-//     mover()
-// });
-
-
-
-
-// setTimeout(function () {
-//     mover();
-// }, 2000);
-
-
-// window.setTimeout(function()
-// {
-//   mover();
-// }, 1000);
